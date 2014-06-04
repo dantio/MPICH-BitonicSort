23ddad837d812d8514a92876fa1e4b8025247fe6
eriCCsan <info@ericwuendisch.de>
2014-06-04 20:37:28 +0200

    new file

---
+++ ParallelBitonicSort.c
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : ParallelBitonicSort.c
+ Author      : Eric Wündisch, Daniil Tomilow
+ ============================================================================
+ */
+#include <stdio.h>
+#include <string.h>
+#include "mpi.h"
+
+int main(int argc, char* argv[]){
+	
+	return 0;
+}
